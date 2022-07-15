27beb68fa0abcbf3cc66e8a74aa8e63aeef23462
Victor Serov <victor.vvs@gmail.com>
2022-07-15 17:37:52 +0300

    Initial Commit

---
+++ simplePlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simplePlayer
+//
+//  Created by Victor Serov on 15/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
